c9afcbc56ec73a98c7a52acd0ef9d8b48096c22c
Gleb Smirnov <me@gvsmirnov.ru>
2014-04-21 01:42:17 +0400

    Initial import

---
+++ src/test/java/ru/gvsmirnov/sample/SampleSmallClassTest.java
@@ -0,0 +1,21 @@
+package ru.gvsmirnov.sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static ru.gvsmirnov.sample.SampleSmallClass.doSomeMath;
+
+public class SampleSmallClassTest {
+
+    @Test
+    public void testMath() {
+final int expected = 0;
+
+for(int a = -5; a <= 5; a ++) {
+    for(int b = -5; b <= 5; b ++) {
+        assertEquals(expected, doSomeMath(a, b));
+    }
+}
+    }
+
+}
