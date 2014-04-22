8d68e9d69463622a35fecb7fc9cc2343f6b36408
Gleb Smirnov <me@gvsmirnov.ru>
2014-04-23 00:59:11 +0400

    Fixed code formatting

--- src/test/java/ru/gvsmirnov/sample/SampleSmallClassTest.java
+++ src/test/java/ru/gvsmirnov/sample/SampleSmallClassTest.java
@@ -9,13 +9,13 @@ public class SampleSmallClassTest {
 
     @Test
     public void testMath() {
-final int expected = 0;
+        final int expected = 0;
 
-for(int a = -5; a <= 5; a ++) {
-    for(int b = -5; b <= 5; b ++) {
-        assertEquals(expected, doSomeMath(a, b));
-    }
-}
+        for(int a = -5; a <= 5; a ++) {
+            for(int b = -5; b <= 5; b ++) {
+                assertEquals(expected, doSomeMath(a, b));
+            }
+        }
     }
 
 }
