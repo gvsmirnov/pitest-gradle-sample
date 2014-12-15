1eafcfdd14b55dd7d1c0af6799d919b2282e2d77
Gleb Smirnov <me@gvsmirnov.ru>
2014-12-15 18:39:19 +0200

    Groovy tests

---
+++ src/test/java/ru/gvsmirnov/sample/SampleGroovyClassJUnitTest.java
@@ -0,0 +1,21 @@
+package ru.gvsmirnov.sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static ru.gvsmirnov.sample.SampleGroovyClass.doSomeMath;
+
+public class SampleGroovyClassJUnitTest {
+
+    @Test
+    public void testMath() {
+        final int expected = 0;
+
+        for(int a = -5; a <= 5; a ++) {
+            for(int b = -5; b <= 5; b ++) {
+                assertEquals(expected, doSomeMath(a, b));
+            }
+        }
+    }
+
+}
---
+++ src/test/java/ru/gvsmirnov/sample/SampleGroovyClassSpockTest.groovy
@@ -0,0 +1,21 @@
+package ru.gvsmirnov.sample
+
+import spock.lang.Specification
+
+import static ru.gvsmirnov.sample.SampleGroovyClass.doSomeMath
+
+/**
+ * @author Gleb Smirnov <me@gvsmirnov.ru>
+ */
+class SampleGroovyClassSpockTest extends Specification {
+
+    def "Math is solid"() {
+        expect:
+          doSomeMath(a, b) == 0
+
+        where:
+          a << (-5..5)
+          b << (-5..5)
+    }
+
+}
