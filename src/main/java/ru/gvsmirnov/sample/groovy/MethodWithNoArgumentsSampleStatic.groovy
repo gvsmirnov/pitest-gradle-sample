03c9c7ab594805ac7cd74476f80cebd0fdd89195
Gleb Smirnov <me@gvsmirnov.ru>
2014-12-20 02:51:59 +0200

    Added sample for methods with no arguments

---
+++ src/main/java/ru/gvsmirnov/sample/groovy/MethodWithNoArgumentsSampleStatic.groovy
@@ -0,0 +1,73 @@
+package ru.gvsmirnov.sample.groovy
+
+import groovy.transform.CompileStatic
+
+/**
+ * @author Gleb Smirnov <me@gvsmirnov.ru>
+ */
+@CompileStatic
+class MethodWithNoArgumentsSampleStatic {
+    public    static final int       PUBLIC_STATIC_FINAL_INT() { 0 }
+              static final int      DEFAULT_STATIC_FINAL_INT() { 0 }
+    private   static final int      PRIVATE_STATIC_FINAL_INT() { 0 }
+    protected static final int    PROTECTED_STATIC_FINAL_INT() { 0 }
+
+    public    static final Object    PUBLIC_STATIC_FINAL_OBJ() { new Object() }
+              static final Object   DEFAULT_STATIC_FINAL_OBJ() { new Object() }
+    private   static final Object   PRIVATE_STATIC_FINAL_OBJ() { new Object() }
+    protected static final Object PROTECTED_STATIC_FINAL_OBJ() { new Object() }
+
+    public    static final def       PUBLIC_STATIC_FINAL_DEF() { new Object() }
+              static final def      DEFAULT_STATIC_FINAL_DEF() { new Object() }
+    private   static final def      PRIVATE_STATIC_FINAL_DEF() { new Object() }
+    protected static final def    PROTECTED_STATIC_FINAL_DEF() { new Object() }
+
+
+    public    static int       PUBLIC_STATIC_INT() { 0 }
+              static int      DEFAULT_STATIC_INT() { 0 }
+    private   static int      PRIVATE_STATIC_INT() { 0 }
+    protected static int    PROTECTED_STATIC_INT() { 0 }
+
+    public    static Object    PUBLIC_STATIC_OBJ() { new Object() }
+              static Object   DEFAULT_STATIC_OBJ() { new Object() }
+    private   static Object   PRIVATE_STATIC_OBJ() { new Object() }
+    protected static Object PROTECTED_STATIC_OBJ() { new Object() }
+
+    public    static def       PUBLIC_STATIC_DEF() { new Object() }
+              static def      DEFAULT_STATIC_DEF() { new Object() }
+    private   static def      PRIVATE_STATIC_DEF() { new Object() }
+    protected static def    PROTECTED_STATIC_DEF() { new Object() }
+
+
+
+    public    final int       PUBLIC_FINAL_INT() { 0 }
+              final int      DEFAULT_FINAL_INT() { 0 }
+    private   final int      PRIVATE_FINAL_INT() { 0 }
+    protected final int    PROTECTED_FINAL_INT() { 0 }
+
+    public    final Object    PUBLIC_FINAL_OBJ() { new Object() }
+              final Object   DEFAULT_FINAL_OBJ() { new Object() }
+    private   final Object   PRIVATE_FINAL_OBJ() { new Object() }
+    protected final Object PROTECTED_FINAL_OBJ() { new Object() }
+
+    public    final def       PUBLIC_FINAL_DEF() { new Object() }
+              final def      DEFAULT_FINAL_DEF() { new Object() }
+    private   final def      PRIVATE_FINAL_DEF() { new Object() }
+    protected final def    PROTECTED_FINAL_DEF() { new Object() }
+
+
+    public    int       PUBLIC_INT() { 0 }
+              int      DEFAULT_INT() { 0 }
+    private   int      PRIVATE_INT() { 0 }
+    protected int    PROTECTED_INT() { 0 }
+
+    public    Object    PUBLIC_OBJ() { new Object() }
+              Object   DEFAULT_OBJ() { new Object() }
+    private   Object   PRIVATE_OBJ() { new Object() }
+    protected Object PROTECTED_OBJ() { new Object() }
+
+    public    def       PUBLIC_DEF() { new Object() }
+              def      DEFAULT_DEF() { new Object() }
+    private   def      PRIVATE_DEF() { new Object() }
+    protected def    PROTECTED_DEF() { new Object() }
+}
